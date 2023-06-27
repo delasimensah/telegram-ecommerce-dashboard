871a2f549c3c8d98fe0e1ddfa620b90ad4080d43
Delasi Mensah <mensadelasi@gmail.com>
2023-06-27 01:44:07 -0700

    updates

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,15 @@
 export type User = {
-  id: number | undefined;
+  id: string | number;
   firstName: string | undefined;
-  lastName: string;
-  username: string;
+  lastName: string | null;
+  username: string | null;
   chatSession?: boolean;
+  currency?: string;
+  paymentMethod?: string;
+  deliveryLocation?: Location;
+  contactNumber?: string;
+  createdAt?: string;
+  blocked?: boolean;
 };
 
 export type Price = {
@@ -19,10 +25,47 @@ export type Product = {
   inStock: boolean;
   category: string;
   prices: Price[];
+  createdAt?: string;
 };
 
 export type Category = {
   id?: string;
   name: string;
   active?: boolean;
+  createdAt?: string;
+};
+
+export type Order = {
+  id?: string | number;
+  userId: string | number;
+  username: string;
+  contactNumber: string;
+  deliveryLocation: Location;
+  paymentMethod: string;
+  fulfilled?: boolean;
+  cancelled?: boolean;
+  products: CartProduct[];
+  total: number;
+  createdAt?: string;
+};
+
+export type CartProduct = {
+  id?: string;
+  name: string;
+  quantity: number;
+  amount: number;
+};
+
+export type Vendor = {
+  id: string | number;
+  currency: string;
+  chatSession: boolean;
+  mobileMoneyNumber?: string;
+  mobileMoneyName?: string;
+  createdAt?: string;
+};
+
+export type Location = {
+  latitude: number;
+  longitude: number;
 };
--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -3,17 +3,77 @@ import { GetServerSideProps } from "next";
 import { useTable } from "@refinedev/react-table";
 import { ColumnDef } from "@tanstack/react-table";
 
-import { List } from "@refinedev/mantine";
+import { List, ShowButton, DeleteButton } from "@refinedev/mantine";
 import { authProvider } from "@lib/authProvider";
 import { Table, Loading, Error, Empty } from "@components";
+import { Group, Text } from "@mantine/core";
+import { CartProduct, Order } from "@lib/types";
+import millify from "millify";
+import { Timestamp } from "firebase/firestore";
+import dayjs from "dayjs";
 
 const ListOrders = () => {
-  const columns = useMemo<ColumnDef<any>[]>(() => {
+  const columns = useMemo<ColumnDef<Order>[]>(() => {
     return [
       {
         id: "id",
-        header: "ID",
+        header: "Order ID",
         accessorKey: "id",
+      },
+      {
+        id: "contactNumber",
+        header: "Contact Number",
+        accessorKey: "contactNumber",
+      },
+      {
+        id: "products",
+        header: "No Of Products",
+        accessorKey: "products",
+        cell: ({ getValue }) => {
+          const value = getValue() as CartProduct[];
+          return <Text>{value.length}</Text>;
+        },
+      },
+      {
+        id: "total",
+        header: "Total",
+        accessorKey: "total",
+        cell: ({ getValue }) => {
+          const value = getValue() as number;
+
+          return (
+            <Text fz="sm" fw={700}>
+              GH₵{" "}
+              {millify(value, {
+                precision: 3,
+              })}
+            </Text>
+          );
+        },
+      },
+      {
+        id: "createdAt",
+        header: "Placed On",
+        accessorKey: "createdAt",
+        cell: ({ getValue }) => {
+          const value = getValue() as string;
+
+          return <Text>{dayjs(value).format("MMM D, YYYY h:mm A")}</Text>;
+        },
+      },
+      {
+        id: "actions",
+        header: "Actions",
+        accessorKey: "id",
+        cell: ({ getValue }) => {
+          const value = getValue() as string;
+          return (
+            <Group spacing="xs" noWrap>
+              <ShowButton variant="subtle" hideText recordItemId={value} />
+              <DeleteButton variant="subtle" hideText recordItemId={value} />
+            </Group>
+          );
+        },
       },
     ];
   }, []);
